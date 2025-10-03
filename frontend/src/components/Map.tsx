c0c0532b3dc496fa7df47ca3deb664b29692d555
R S Chiraag <chiraag.rs@gmail.com>
2025-10-03 18:07:40 +0530

    Add tailwindcss and basic ui for issue popup

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,19 +1,16 @@
-import {
-	MapContainer,
-	TileLayer,
-	Marker,
-	Popup,
-	useMapEvents,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MapClickHandler from "../features/MapClickHandler";
+import { IssueCard } from "../features/IssueCard";
+import type { Issue } from '../types/issue';
 
-const sampleIssues = [
+const sampleIssues: Issue[] = [
 	{
 		id: 1,
 		lat: 12.97615,
 		lng: 77.60387,
 		title: "Pothole",
 		description: "Large pothole on main road",
+        imgUrl: 'https://media.newyorker.com/photos/62e9890ed845dffa5de16b9a/master/pass/Keeley-Sinkhole-1_1.jpg'
 	},
 	{
 		id: 2,
@@ -21,6 +18,7 @@ const sampleIssues = [
 		lng: 77.61964,
 		title: "Flood",
 		description: "Area full flooded",
+        imgUrl: 'https://i.redd.it/ulsoor-lake-good-morning-bengaluru-v0-rrw6izuon9991.jpg?width=4032&format=pjpg&auto=webp&s=ec36772165e936208a2e1d23a01b0cef4d7f5ab3'
 	},
 ];
 
@@ -49,12 +47,10 @@ const Map = () => {
 				url="https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/{z}/{y}/{x}"
 			/>
 			<MapClickHandler onClick={handleMapClick} />
-			{sampleIssues.map((issue) => (
+			{sampleIssues.map((issue: Issue) => (
 				<Marker key={issue.id} position={[issue.lat, issue.lng]}>
 					<Popup>
-						<strong>{issue.title}</strong>
-						<br />
-						{issue.description}
+						<IssueCard issue={issue} />
 					</Popup>
 				</Marker>
 			))}
---
+++ frontend/src/features/IssueCard.tsx
@@ -0,0 +1,36 @@
+// import React from "react";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import type { Issue } from "../types/issue";
+
+interface IssueCardProps {
+	issue: Issue;
+}
+
+export const IssueCard = ({ issue }: IssueCardProps) => {
+	return (
+		<div className="max-w-sm bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition-all duration-300">
+			<div className="relative overflow-hidden h-48 bg-gradient-to-br from-emerald-50 to-teal-50">
+				<img
+					src={issue.imgUrl}
+					alt="Issue preview"
+					className="w-full h-full object-cover"
+				/>
+			</div>
+
+			<div className="p-6">
+				<h3 className="text-xl font-bold text-gray-800 mb-3 hover:text-emerald-700 transition-colors">
+					{issue.title}
+				</h3>
+
+				<p className="text-gray-600 text-sm leading-relaxed mb-5">
+					{issue.description}
+				</p>
+
+				<button className="group w-full bg-emerald-700 hover:bg-emerald-800 text-white font-medium py-3 px-4 rounded-lg flex items-center justify-center gap-2 transition-all duration-200 shadow-md cursor-pointer">
+					<span>View Issue</span>
+                    <FaArrowUpRightFromSquare />
+				</button>
+			</div>
+		</div>
+	);
+};
